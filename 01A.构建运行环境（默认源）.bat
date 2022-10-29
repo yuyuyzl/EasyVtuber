d1d88afb9bcca71c7f9d7d5d0b6b50f46375619f
yuyuyzl <yuzhongliang221@gmail.com>
2022-10-29 10:27:53 +0800

    realesrgan

--- 01A.构建运行环境（默认源）.bat
+++ 01A.构建运行环境（默认源）.bat
@@ -2,4 +2,5 @@
 cd bin
 python.exe -m pip install -r ..\requirements.txt --no-warn-script-location
 python.exe -m pip install torch --extra-index-url https://download.pytorch.org/whl/cu113 --no-warn-script-location
+python.exe -m pip install basicsr --no-warn-script-location
 pause
--- 01B.构建运行环境（国内源）.bat
+++ 01B.构建运行环境（国内源）.bat
@@ -2,4 +2,5 @@
 cd bin
 python.exe -m pip install -r ..\requirements.txt -i https://mirror.sjtu.edu.cn/pypi/web/simple --no-warn-script-location
 python.exe -m pip install torch --extra-index-url https://mirror.sjtu.edu.cn/pytorch-wheels/cu113 --no-warn-script-location
+python.exe -m pip install basicsr -i https://mirror.sjtu.edu.cn/pypi/web/simple --no-warn-script-location
 pause
